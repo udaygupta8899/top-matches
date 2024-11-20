f70af0e335aa84861e7788940884d761e39b0aa2
udaygupta8899 <149859386+udaygupta8899@users.noreply.github.com>
2024-11-20 12:00:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -76,7 +76,7 @@ if uploaded_file is not None:
     user_input = {}
     for col in data.columns:
         if data[col].dtype in ['int64', 'float64']:
-            user_input[col] = st.number_input(f"Enter value for {col}", value=0)
+            user_input[col] = st.number_input(f"Enter value for {col}", value=1,min_value=1,max_value=10)
         else:
             user_input[col] = st.selectbox(f"Select value for {col}", options=data[col].unique())
 
